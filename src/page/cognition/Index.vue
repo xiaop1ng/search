fb110a43b810d2a38d870c8d96f12026f9edc1d9
xiaop1ng <784516419@qq.com>
2019-04-03 21:06:34 +0800

    es

--- src/page/cognition/Index.vue
+++ src/page/cognition/Index.vue
@@ -1,20 +1,20 @@
 <template>
     <el-main style="min-height: 600px;">
-      <!-- 添加 -->
-      <el-dialog title="添加文档" :visible.sync="dialogAddFormVisible">
-        <el-form :model="addForm">
-          <el-form-item label="标题" :label-width="formLabelWidth">
-            <el-input v-model="addForm.title" autocomplete="off"></el-input>
-          </el-form-item>
-          <el-form-item label="内容" :label-width="formLabelWidth">
-            <el-input v-model="addForm.body"  type="textarea" :autosize="{ minRows: 10, maxRows: 40}" autocomplete="off"></el-input>
-          </el-form-item>
-        </el-form>
-        <div slot="footer" class="dialog-footer">
-          <el-button @click="dialogFormVisible = false">取 消</el-button>
-          <el-button type="primary" @click="submitAddForm">确 定</el-button>
-        </div>
-      </el-dialog>
+        <!-- 添加、修改、查看 -->
+        <el-dialog :title="dialogTitle" :visible.sync="dialogAddFormVisible">
+          <el-form :model="addForm">
+            <el-form-item label="标题">
+              <el-input v-model="addForm.title" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="内容">
+              <el-input v-model="addForm.body"  type="textarea" :autosize="{ minRows: 10, maxRows: 40}" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogAddFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="submitAddForm">确 定</el-button>
+          </div>
+        </el-dialog>
 
         <el-table
             ref="multipleTable"
@@ -27,17 +27,17 @@
             width="55">
             </el-table-column>
             <el-table-column
-            label="日期"
+            label="时间戳"
             width="200">
-            <template slot-scope="scope">{{ scope.row.date }}</template>
+            <template slot-scope="scope">{{ scope.row._source.date }}</template>
             </el-table-column>
             <el-table-column
-            prop="title"
+            prop="_source.title"
             label="标题"
             width="300">
             </el-table-column>
             <el-table-column
-            prop="desc"
+            prop="_source.body"
             label="内容"
             show-overflow-tooltip>
             </el-table-column>
@@ -74,7 +74,11 @@
       return {
         tableData: [],
         dialogAddFormVisible: false,
-        addForm: {}
+        addForm: {},
+        pageNo: 1,
+        pageSize: 20,
+        dialogTitle: '',
+        op: 1, // 1 添加； 2 修改； 3 查看
       }
     },
     computed: {
@@ -82,35 +86,77 @@
     },
     mounted() {
       console.debug("mounted")
-      var res = cognitionService.list()
-      console.log(res)
-      if(res.hits) {
-        // 存在数据
-      }
+      this.pageInit()
     },
     methods: {
+      pageInit: function() {
+        var _this = this
+        const loading = this.$loading({
+          lock: true,
+          // text: 'Loading',
+          // spinner: 'el-icon-loading',
+          // background: 'rgba(0, 0, 0, 0.7)'
+        })
+        setTimeout( async () => {
+          loading.close();
+          var res = await cognitionService.list(this.pageNo, this.pageSize)
+          _this.tableData = res.data.hits.hits
+        }, 980)
+        
+      },
       handleSelectionChange: function(res) {
           console.debug('seleChange: ', res)
       },
+      getDialogTitle: function() {
+        if(this.op == 1) {
+          return '添加文档'
+        } else if(this.op == 2) {
+          return '修改文档'
+        } else if (this.op == 3) {
+          return '查看文档'
+        } else {
+          return '文档'
+        }
+      },
       handCreate: function() {
         // 弹窗
+        this.addForm = {}
+        this.op = 1
+        this.dialogTitle = this.getDialogTitle()
         this.dialogAddFormVisible = true
       },
       handleEdit: function(idx, row) {
-
+        this.addForm = row._source
+        this.addForm.id = row._id
+        this.op = 2
+        this.dialogTitle = this.getDialogTitle()
+        this.dialogAddFormVisible = true
       },
-      handleDelete: function(idx, row) {
-
-      },
-      submitAddForm: function() {
-        console.log(this.addForm)
-        this.dialogAddFormVisible = false
-        var res = cognitionService.upsert(this.addForm)
-        if(res && res._id) {
+      handleDelete: async function(idx, row) {
+        var res = await cognitionService.del(row._id)
+        if(res && res.data.result == "deleted") {
+          
           this.$message({
             message: 'success',
             type: 'success'
           });
+          this.pageInit()
+          // var _this = this
+          // window.setTimeout(function() {
+          //   _this.pageInit()
+          // }, 1000)
+        }
+      },
+      submitAddForm: async function() {
+        this.dialogAddFormVisible = false
+        this.addForm.date = new Date().getTime()
+        var res = await cognitionService.upsert(this.addForm)
+        if(res && res.data._id) {
+          this.$message({
+            message: 'success',
+            type: 'success'
+          });
+          this.pageInit()
         }
       }
     }
--- src/service/cognitionService.js
+++ src/service/cognitionService.js
@@ -6,12 +6,20 @@ const cognitionApi = '/doc/cognition'
 const http = axios.create({
     baseURL: baseURL,
     timeout: 20000,
-    headers : {'Content-Type': 'application/json'}
+    headers : {
+        'Content-Type': 'application/json',
+        // 'Cache-Control': 'no-cache'
+    }
 });
 
 // 创建或更新文档
 export const upsert = async (obj) => {
-    const res = await http.post(cognitionApi, obj)
+    var opStr = ''
+    if(obj.id) {
+        opStr = '/' + obj.id
+        delete obj.id
+    }
+    const res = await http.post(cognitionApi + opStr, obj)
     return res
 }
 
@@ -26,7 +34,8 @@ export const list = async (pageNo, pageSize) => {
     var param = {
 
     }
-    const res = await http.get(cognitionApi + '/_search', param);
+    const res = await http.get(cognitionApi + '/_search', param)
+    console.debug("res", res)
     return res
 }
 
